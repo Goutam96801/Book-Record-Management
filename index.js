8040092f112c2c8de78b16023059d88e9b871387
Goutam96801 <gautamkumar96801@gmail.com>
2023-04-02 02:02:46 +0530

    refactored code and added some book routes

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 
+
+//importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
+
 const app = express();
 
 const PORT = 8081;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.status(200).json({
         message: "Server is up and running",
     });
 });
+
+
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
+
 
 app.get("*", (req, res) => {
     res.status(200).json({
@@ -21,3 +31,4 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
 });
+
